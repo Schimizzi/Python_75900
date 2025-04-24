9cb47dd1424ef9d8109b7fdc85cb59007de9d86f
Schimizzi <bastaparavos@hotmail.com>
2025-04-24 12:28:13 -0300

    lee_json

--- biblioteca.py
+++ biblioteca.py
@@ -1,6 +1,7 @@
 import json
 from cliente import Cliente
 from libro import Libro
+import os
 
 class Biblioteca:
     def __init__(self):
@@ -33,10 +34,30 @@ class Biblioteca:
 
 
 if __name__ == '__main__':
+
+    # Construir la ruta del archivo JSON de libros
+    libros_path = os.path.join('entrega2', 'data_libros.json')
+
+    # Leer datos de libros desde el archivo JSON y guardarlos en una variable
+    with open(libros_path, 'r') as fl:
+        libros_data = json.load(fl)
+
+    # Construir la ruta del archivo JSON de libros
+    cliente_path = os.path.join('entrega2', 'data_clientes.json')
+
+    # Leer datos de libros desde el archivo JSON y guardarlos en una variable
+    with open(cliente_path, 'r') as fc:
+        cliente_path = json.load(fc)
+
+
+    # Imprimir los datos de libros para verificar
+    print(libros_data)
+    print(cliente_path)
+
     biblioteca = Biblioteca()
 
-    cliente1 = Cliente(1, "claudio" , 29000999, 11336699)
-    cliente2 = Cliente(2, "pepe" , 29000888, 11226699)
+    cliente1 = Cliente(1, "claudio", 29000999, 11336699)
+    cliente2 = Cliente(2, "pepe", 29000888, 11226699)
     libro1 = Libro(3, "Pedro", "Aprendiendo Python")
     libro2 = Libro(4, "Pablo", "Aprende a colorear desde cero")
     print(libro1)
@@ -48,5 +69,3 @@ if __name__ == '__main__':
     biblioteca.agregar_clientes(cliente2)
     biblioteca.imprimir_libros()
     biblioteca.imprimir_clientes()
-
-
