6842f87f3e535822a5f8727f223c9c4862c3830b
Schimizzi Claudio Roberto <schimizzic@tasa.telefonica.com.ar>
2025-04-29 11:07:53 -0300

    add_setup.py_exec

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sistema-gestion-libreria",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Schimizzi, Claudio",
+    author_email="bastaparavos@hotmail.com",
+    description="Sistema básico para gestionar libros y clientes de una librería en Python.",
+    url="https://github.com/Schimizzi/Python_75900.git",
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=[],
+    keywords="libreria, libros, clientes, gestion, python",
+)
